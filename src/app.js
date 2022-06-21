13b2809a10705a3b8287b2a6bb48fed101520a97
Raffaele <raffaeleoni@users.noreply.github.com>
2022-06-21 21:18:38 +0200

    TC docker compose up and running

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-console.log("args passed", process.argv.splice(2));
+console.log("args passed", process.argv.splice(2), "\n\n", process.env.NODE_ENV, process.env.API_KEY );
+
+
+const mongoose = require('mongoose');
